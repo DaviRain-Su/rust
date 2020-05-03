c980d28f4db458fea6461238643af6102bf09718
DaviRain-Su <davirain.yin@gmail.com>
2020-05-03 11:21:53 +0800

    ...

---
+++ communicator/target/debug/deps/communicator-afa2c7e49d884e3c.d
@@ -0,0 +1,5 @@
+/Users/suyinrong/rust/communicator/target/debug/deps/communicator-afa2c7e49d884e3c.rmeta: src/lib.rs
+
+/Users/suyinrong/rust/communicator/target/debug/deps/communicator-afa2c7e49d884e3c.d: src/lib.rs
+
+src/lib.rs:
